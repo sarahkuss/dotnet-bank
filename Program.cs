6f3021803eb1deb526b63fe89f53f11aea8796c6
Sarah Kuss <sarah.kuss33@gmail.com>
2023-05-09 14:40:29 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Console.WriteLine("Hello, World!");
+
+int age = 27;
+string name = "Sarah";
+bool isHere = true;
+//int salary = 22000;
+double raise = 22000.01; //double lets you use decimals
+
+Console.WriteLine($"Hi, {name}. Your age is {age} and is here? {isHere}");
+Console.WriteLine($"And you make ${raise} a year");
+
